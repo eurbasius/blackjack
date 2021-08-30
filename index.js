23941d6c7ea43efd72e8196c694d192ddd6d20c6
eurbasius <eur@whiteaway.com>
2021-08-30 07:47:08 +0200

    added cards display

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ let hasBlackJack = false;
 let isAlive = true;
 let resultEl = document.getElementById("resultEl");
 let sumEl = document.getElementById("sumEl");
+let cardsEl = document.getElementById("cardsEl");
 
 function startGame() {
-  sumEl.textContent = `Sum: ${sum}`
+  sumEl.textContent = `Sum: ${sum}`;
+  cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
   if (sum <= 20) {
-      resultEl.textContent = "Please, take another card!"
+      resultEl.textContent = "Please, take another card!";
   } else if (sum === 21) {
-      resultEl.textContent = "Congratulations, you won!"
+      resultEl.textContent = "Congratulations, you won!";
       hasBlackJack = true;
   } else {
-      resultEl.textContent = "You lost!"
+      resultEl.textContent = "You lost!";
       isAlive = false;
   }
 }
