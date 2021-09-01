d3d33e349ef6bdc45e4bcd856f3414077e5a4d5f
eurbasius <eur@whiteaway.com>
2021-09-01 20:14:35 +0200

    generate random card

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 9;
-let secondCard = 2;
+let firstCard = generateRandomCard();
+let secondCard = generateRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let message;
@@ -9,13 +9,21 @@ let resultEl = document.getElementById("resultEl");
 let sumEl = document.getElementById("sumEl");
 let cardsEl = document.getElementById("cardsEl");
 
+function generateRandomCard() {
+  return 5;
+}
+
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
   sumEl.textContent = `Sum: ${sum}`;
-  cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`;
+  cardsEl.textContent = "Cards: ";
+  // Create a for loop that renders out all the cards instead of just two
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
   // Render out all of the cards!
   if (sum <= 20) {
     resultEl.textContent = "Please, take another card!";
@@ -29,9 +37,9 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log("Drawing a new card from the deck!");
-    let card = 2;
-    sum += card;
-    cards.push(card);
-    renderGame();
+  console.log("Drawing a new card from the deck!");
+  let card = generateRandomCard();
+  sum += card;
+  cards.push(card);
+  renderGame();
 }
