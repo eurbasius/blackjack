41e310ae6edc6fa0a9fa255cbfd3c87ad227af87
eurbasius <eur@whiteaway.com>
2021-09-01 08:31:31 +0200

    adding cards array.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 9;
 let secondCard = 2;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let message;
 let hasBlackJack = false;
@@ -9,15 +10,28 @@ let sumEl = document.getElementById("sumEl");
 let cardsEl = document.getElementById("cardsEl");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
   sumEl.textContent = `Sum: ${sum}`;
-  cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
+  cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`;
+  // Render out all of the cards!
   if (sum <= 20) {
-      resultEl.textContent = "Please, take another card!";
+    resultEl.textContent = "Please, take another card!";
   } else if (sum === 21) {
-      resultEl.textContent = "Congratulations, you won!";
-      hasBlackJack = true;
+    resultEl.textContent = "Congratulations, you won!";
+    hasBlackJack = true;
   } else {
-      resultEl.textContent = "You lost!";
-      isAlive = false;
+    resultEl.textContent = "You lost!";
+    isAlive = false;
   }
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    let card = 2;
+    sum += card;
+    cards.push(card);
+    renderGame();
 }
